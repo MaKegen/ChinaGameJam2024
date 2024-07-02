28c2f646853d090f13f6c01db3cd3085d67bf2a4
MaKegen <fengheyin@gmail.com>
2024-07-02 20:47:30 +0800

    this movement code got some problems

--- Assets/Tests/Character_Movement_Controller.cs
+++ Assets/Tests/Character_Movement_Controller.cs
@@ -1,18 +1,29 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine.Utility;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
-public class Animation_Movement_Controller : MonoBehaviour
+public class Character_Movement_Controller : MonoBehaviour
 {
     InputActions PlayerInput;
     Animator animator;
     private bool IsCrouching;
     private bool IsRunning;
+    private Vector2 InputDirection;
+    private Vector3 MoveDirection;
+    private Quaternion CurrentRotation;
+    [SerializeReference]private float RotationPerFrame;
+    private Vector3 RotationDestination;
+    [SerializeField]private float MovementSpeed;
+    [SerializeField]private float RunningSpeed;
+    private CharacterController player;
     private void Awake() {
         PlayerInput = new InputActions();
-        animator = GetComponent<Animator>();
+        animator = GetComponentInChildren<Animator>();
+        player = GetComponent<CharacterController>();
     }
     // Start is called before the first frame update
 
@@ -56,10 +67,19 @@ public class Animation_Movement_Controller : MonoBehaviour
     {
         IsCrouching = animator.GetBool("IsCrouching");
         IsRunning = animator.GetBool("IsRunning");
+        if(IsRunning){
+            player.Move(MoveDirection*RunningSpeed*Time.deltaTime);
+        }
+        else{
+            player.Move(MoveDirection*Time.deltaTime);
+        }
+        HandleRotation();
+        
     }
     private void OnRunning(InputAction.CallbackContext context)
     {
         Debug.Log("跑"+context.ReadValueAsButton());
+
         if(context.started){
             animator.SetBool("IsRunning",!IsRunning);
             animator.SetBool("IsCrouching",false);
@@ -78,6 +98,8 @@ public class Animation_Movement_Controller : MonoBehaviour
     private void OnMove(InputAction.CallbackContext context)
     {
         Debug.Log("在运动"+context.ReadValue<Vector2>());
+        InputDirection = context.ReadValue<Vector2>();
+        MoveDirection = new Vector3(InputDirection.x,0,InputDirection.y)*MovementSpeed;
         if(context.started|context.performed){
             animator.SetBool("IsWalking",true);
         }
@@ -86,5 +108,13 @@ public class Animation_Movement_Controller : MonoBehaviour
             animator.SetBool("IsRunning",false);
             
         }
+    }
+    private void HandleRotation(){
+        RotationDestination.x = InputDirection.x;
+        RotationDestination.y = 0f;
+        RotationDestination.z = InputDirection.y;
+        CurrentRotation = transform.rotation;
+        Quaternion TargetRotation = Quaternion.LookRotation(RotationDestination);
+        transform.rotation = Quaternion.Slerp(CurrentRotation,TargetRotation,RotationPerFrame*Time.deltaTime);
     }
 }
