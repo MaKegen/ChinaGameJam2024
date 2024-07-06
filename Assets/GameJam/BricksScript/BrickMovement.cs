6a47133f715f133df4deaec6e5678f952866b023
MaKegen <fengheyin@gmail.com>
2024-07-06 21:41:33 +0800

    砖块搞定两个

--- Assets/GameJam/BricksScript/BrickMovement.cs
+++ Assets/GameJam/BricksScript/BrickMovement.cs
@@ -72,6 +72,85 @@ public class BrickMovement : MonoBehaviour
         transform.position += Vector3.down;
         if(!Moveable){
             transform.position += Vector3.up;
+            Land();
+            ClearFullRows();
+            enabled = false;
+        }
+    }
+     void Land()
+    {
+        foreach (Transform child in transform)
+        {
+            int x = Mathf.RoundToInt(child.position.x);
+            int y = Mathf.RoundToInt(child.position.y);
+
+            grid[x, y] = child;
+
+            if (y == HEIGHT - 1)
+            {
+                //GameOver
+            }
+        }
+    }
+    void ClearFullRows()
+    {
+        // check rows from top to bottom
+        for (int y = HEIGHT - 1; y >= 0; y--)
+        {
+            // if row y is full 
+            if (IsRowFull(y))
+            {
+                // TODO: spawn SFX here
+                // destroy the full row
+                DestroyRow(y);
+                // decrease the rows above
+                DecreaseRow(y);
+            }
+        }
+    }
+
+    bool IsRowFull(int y)
+    {
+        // check grids in a row if full
+        for (int x = 0; x < WIDTH; x++)
+        {
+            if (grid[x, y] == null)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void DestroyRow(int y)
+    {
+        for (int x = 0; x < WIDTH; x++)
+        {
+            Destroy(grid[x, y].gameObject);
+            // after destroy the block set this transform null
+            grid[x, y] = null;
+        }
+    }
+
+    void DecreaseRow(int row)
+    {
+        // after clear a full row, decrease rows above
+        for (int y = row; y < HEIGHT - 1; y++)
+        {
+            for (int x = 0; x < WIDTH; x++)
+            {
+                // if the row above has a block
+                if (grid[x, y + 1] != null)
+                {
+                    // switch the transforms of this row and the row above 
+                    grid[x, y] = grid[x, y + 1];
+                    // it should be null because the transform was swapped
+                    grid[x, y + 1] = null;
+                    // move the swapped blocks down
+                    grid[x, y].gameObject.transform.position += Vector3.down;
+                }
+            }
         }
     }
 
